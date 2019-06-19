343265310af082047229190ef14060b3d758a8e7
piotr <piotrborciuch@gmail.com>
2019-06-19 16:57:44 +0200

    Second Stage Commit

--- Coffee Machine/task/src/machine/CoffeeMachine.java
+++ Coffee Machine/task/src/machine/CoffeeMachine.java
@@ -1,13 +1,30 @@
 package machine;
 
+import java.util.Scanner;
+
 public class CoffeeMachine {
     public static void main(String[] args) {
-        System.out.println("Starting to make a coffee");
-        System.out.println("Grinding coffee beans");
-        System.out.println("Boiling water");
-        System.out.println("Mixing boiled water with crushed coffee beans");
-        System.out.println("Pouring coffee into the cup");
-        System.out.println("Pouring some milk into the cup");
-        System.out.println("Coffee is ready!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Write how many cups of coffee you will need:");
+        int cupsOfCoffee = scanner.nextInt();
+        System.out.println(String.format("For %d cups of coffee you will need:", cupsOfCoffee));
+        System.out.println(String.format("%d ml of water",countNeededWater(cupsOfCoffee)));
+        System.out.println(String.format("%d ml of milk", countNeededMilk(cupsOfCoffee)));
+        System.out.println(String.format("%d g of coffee bean", countNee1dedCoffeeBeans(cupsOfCoffee)));
+
     }
+
+    public static int countNeededWater(int cupsOfCoffee){
+        return cupsOfCoffee * 200;
+    }
+
+    public static int countNeededMilk(int cupsOfCoffee){
+        return cupsOfCoffee * 50;
+    }
+
+    public static int countNee1dedCoffeeBeans(int cupsOfCoffee){
+        return cupsOfCoffee * 15;
+    }
+
 }
